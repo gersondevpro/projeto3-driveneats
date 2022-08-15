8634255ec3e9ba9d382d3b9bfad02a1b0d26ddcf
Gerson Goncalves <gersondevpro@gmail.com>
2022-08-15 16:00:04 -0300

    Alteração HTML, CSS e JS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let burgers = false;
+let precoBurgers = 0;
+
 function escolherBurger(burger) {
     const selecao = document.querySelector('.burgers .selecionar');
 
@@ -7,13 +9,16 @@ function escolherBurger(burger) {
     }
 
     burger.classList.add('selecionar');
-    burgers = true;
+    burgers = burger;
+    precoBurgers = Number(burger.getElementsByTagName("h3")[0].textContent);
 
     finalizar();
 }
 
 
 let bebidas = false;
+let precoBebidas = 0;
+
 function escolherBebida(bebida) {
     const selecao = document.querySelector('.bebidas .selecionar');
 
@@ -22,12 +27,16 @@ function escolherBebida(bebida) {
     }
 
     bebida.classList.add('selecionar');
-    bebidas = true;
+    bebidas = bebida;
+    precoBebidas = Number(bebidas.getElementsByTagName("h3")[0].textContent);
 
     finalizar();
 }
 
+
 let sobremesas = false;
+let precoSobremesas = 0;
+
 function escolherSobremesa(sobremesa) {
     const selecao = document.querySelector('.sobremesas .selecionar');
 
@@ -36,19 +45,24 @@ function escolherSobremesa(sobremesa) {
 
     }
     sobremesa.classList.add('selecionar');
-    sobremesas = true;
+    sobremesas = sobremesa;
+    precoSobremesas = Number(sobremesas.getElementsByTagName("h3")[0].textContent);
 
     finalizar();
 }
 
-
 function finalizar() {
     
-    const processarPedido = document.querySelector(".condition");
-    
     if(bebidas && burgers && sobremesas) {
+        const processarPedido = document.querySelector(".condition");
         processarPedido.classList.add("enviarPedido");
-        processarPedido.innerHTML = "Finalizar pedido";
+        let preco = precoBebidas + precoBurgers + precoSobremesas;
+
+        let msgWhats = `Olá, gostaria de fazer o pedido: \n- Prato: ${burgers.getElementsByTagName("h1")[0].textContent} \n- Bebida: ${bebidas.getElementsByTagName("h1")[0].textContent} \n- Sobremesa: ${sobremesas.getElementsByTagName("h1")[0].textContent} \nTotal: R$ ${preco.toFixed(2)}`;
+
+
+        let encode = encodeURIComponent(msgWhats);
+        processarPedido.innerHTML = `<a href="https://wa.me/5512992103300?text=${msgWhats}"><p>Fechar pedido</p></a>`
     }
     
 }
